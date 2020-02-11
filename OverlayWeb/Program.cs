0248e6e3f9b7d4131f0375c79633041d20ff0df2
Bram Hagens <bramhagens@hotmail.nl>
2020-02-11 17:59:59 +0100

    Add iRacingSdkWrapper

--- OverlayWeb/Program.cs
+++ OverlayWeb/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using OverlayWeb.Data;
 
 namespace OverlayWeb
 {
@@ -13,6 +14,7 @@ namespace OverlayWeb
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton<SdkService>();
 
             await builder.Build().RunAsync();
         }
